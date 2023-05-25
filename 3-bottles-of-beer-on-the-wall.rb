87d271172487a8a8079f99af6e48a569f641337b
PhilipeeX <dev.philipe@outlook.com>
2023-05-25 13:57:34 -0300

    Exercício 3 terminado

--- 3-bottles-of-beer-on-the-wall.rb
+++ 3-bottles-of-beer-on-the-wall.rb
@@ -1,1 +1,16 @@
 # Define a recursive function that takes an argument n and prints "n bottles of beer on the wall", "(n-1) bottles of beer on the wall", ..., "no more bottles of beer on the wall".
+
+def bottles_of_beer(n_times)
+  if n_times == 0
+    puts "no more bottles of beer on the wall"
+  elsif n_times == 1
+    puts "#{n_times} bottle of beer on the wall"
+    bottles_of_beer(n_times - 1)
+  else
+    puts "#{n_times} bottles of beer on the wall"
+    bottles_of_beer(n_times - 1)
+  end
+end
+
+puts "How many bottles of beer on the wall?"
+bottles_of_beer(gets.chomp.to_i)
