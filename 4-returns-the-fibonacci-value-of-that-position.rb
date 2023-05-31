5d4ca3c298e2722ea104134b6cb10a491c1e1fc5
PhilipeeX <dev.philipe@outlook.com>
2023-05-31 09:31:09 -0300

    Tentativa falha de resolver o exercício número 4, tentar resolver posteriormente

--- 4-returns-the-fibonacci-value-of-that-position.rb
+++ 4-returns-the-fibonacci-value-of-that-position.rb
@@ -1,39 +1,11 @@
 # Define a recursive function that takes an argument n and returns the fibonacci value of that position. The fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21... So fib(5) should return 5 and fib(6) should return 8.
 
 def fibonacci_position(valor)
-  vezes = 0
-  x = 0
-  y = 1
-
-  while vezes < valor
-    if vezes % 2 == 0
-      x = y + x
-    else
-      y = y + x
-    end
-    vezes += 1
-  end
-
-
-end
-
-def fibonacci_position(valor)
-  binding.pry
+  # caso base
   if valor == 0
-    x = 0
-    y = 1
+    return 0
   else
-    fibonacci_position(valor - 1)
-  end
-  
-  if valor > 0
-    x = x + y
-  end
-
-  if valor % 2 == 0
-    return x
-  else 
-    return y
+    x = fibonacci_position(valor - 1)
   end
 
 end
